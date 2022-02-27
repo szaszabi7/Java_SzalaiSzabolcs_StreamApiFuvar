462c2b3e94c49791b9651a951b5ff091d0966d0b
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-02-27 17:28:40 +0100

    az előző commit a feladat 11

--- src/hu/petrik/streamapifuvar/Main.java
+++ src/hu/petrik/streamapifuvar/Main.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     private static List<Fuvar> fuvarLista;
