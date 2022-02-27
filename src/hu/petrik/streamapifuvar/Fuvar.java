aec2222b5358cdfe6a5e7a1d39044ad587dc3081
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-02-27 15:52:13 +0100

    fuvar class, beolvasas, feladat1

---
+++ src/hu/petrik/streamapifuvar/Fuvar.java
@@ -0,0 +1,91 @@
+package hu.petrik.streamapifuvar;
+
+public class Fuvar {
+    private int taxi_id;
+    private String indulas;
+    private int idotartam;
+    private double tavolsag;
+    private double viteldij;
+    private double borravalo;
+    private String fizetes_modja;
+
+    public int getTaxi_id() {
+        return taxi_id;
+    }
+
+    public void setTaxi_id(int taxi_id) {
+        this.taxi_id = taxi_id;
+    }
+
+    public String getIndulas() {
+        return indulas;
+    }
+
+    public void setIndulas(String indulas) {
+        this.indulas = indulas;
+    }
+
+    public int getIdotartam() {
+        return idotartam;
+    }
+
+    public void setIdotartam(int idotartam) {
+        this.idotartam = idotartam;
+    }
+
+    public double getTavolsag() {
+        return tavolsag;
+    }
+
+    public void setTavolsag(double tavolsag) {
+        this.tavolsag = tavolsag;
+    }
+
+    public double getViteldij() {
+        return viteldij;
+    }
+
+    public void setViteldij(double viteldij) {
+        this.viteldij = viteldij;
+    }
+
+    public double getBorravalo() {
+        return borravalo;
+    }
+
+    public void setBorravalo(double borravalo) {
+        this.borravalo = borravalo;
+    }
+
+    public String getFizetes_modja() {
+        return fizetes_modja;
+    }
+
+    public void setFizetes_modja(String fizetes_modja) {
+        this.fizetes_modja = fizetes_modja;
+    }
+
+    public Fuvar(String sor) {
+        String[] adat = sor.replace(',', '.').split(";");
+
+        this.taxi_id = Integer.parseInt(adat[0]);
+        this.indulas = adat[1];
+        this.idotartam = Integer.parseInt(adat[2]);
+        this.tavolsag = Double.parseDouble(adat[3]);
+        this.viteldij = Double.parseDouble(adat[4]);
+        this.borravalo = Double.parseDouble(adat[5]);
+        this.fizetes_modja = adat[6];
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-6d: %-22s %-5d %-5f mérföld %-5f$ %-5f$ %-10s",
+                this.taxi_id,
+                this.indulas,
+                this.idotartam,
+                this.tavolsag,
+                this.viteldij,
+                this.borravalo,
+                this.fizetes_modja);
+    }
+}
---
+++ src/hu/petrik/streamapifuvar/Main.java
@@ -0,0 +1,42 @@
+package hu.petrik.streamapifuvar;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static List<Fuvar> fuvarLista;
+
+    public static void main(String[] args) {
+        beolvasas("fuvar.csv");
+
+        feladat1();
+    }
+
+    private static void beolvasas(String file) {
+        fuvarLista = new ArrayList<>();
+
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            br.readLine();
+            String sor = br.readLine();
+            while (sor != null) {
+                fuvarLista.add(new Fuvar(sor));
+                sor = br.readLine();
+            }
+
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void feladat1() {
+        System.out.println("1. feladat: " + fuvarLista.stream().count() + " db utazás került feljegyzésre");
+    }
+}
